cf6b5bc252831d1d53750fc6a0459610185e7bbd
Jacob <malin146@umn.edu>
2024-11-02 00:23:36 -0500

    The Black Prism

--- src/read.py
+++ src/read.py
@@ -1,25 +1,32 @@
 import pymupdf
 import re
 
-book = "books/The Raven Boys.pdf"
-preface_end_page = 5
+book = "books/The Black Prism.pdf"
+preface_end_page = 9
+postface_skip_count = 4
 chapter = 0
 
 doc = pymupdf.open(book) # open a document
 out = open("output.txt", "wb") # create a text output
-out.write('<p>'.encode('utf8'))
-for page in doc[preface_end_page:]: # iterate the document pages
+out.write('<p>\n'.encode('utf8'))
+count = 0
+for page in doc[preface_end_page:-postface_skip_count]: # iterate the document pages
     block = page.get_text("blocks")
     for b in block:
         text = b[4]
 
-        if re.fullmatch("\d+ \n", text): continue
-        if " \n \n \n" in text: chapter += 1
-        text = text.replace(" \n \n \n", "</p>\0<p>\0CHAPTER {}\0</p>\0<p>".format(chapter))
-        text = text.replace(" \n \n", "\0</p>\0<p>\0")
+        if re.fullmatch("Chapter \d+\n", text):
+            count = 0
+        # if " \n \n \n" in text: chapter += 1
+        # text = text.replace(" \n \n \n", "</p>\0<p>\0CHAPTER {}\0</p>\0<p>".format(chapter))
+        # text = text.replace(" \n \n", "\0</p>\0<p>\0")
         text = text.replace('\n', '')
-        text = text.replace('\0', '\n')
-
+        # text = text.replace('\0', '\n')
+        
+        if (text[0].isupper()):
+            out.write('\n</p>\n'.encode('utf8')) # write text of page
+            out.write('\n<p>\n'.encode('utf8')) # write text of page
         out.write(text.encode('utf8')) # write text of page
+        count += 1
 out.write('</p>'.encode('utf8'))
 out.close()
