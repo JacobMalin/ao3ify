3a9001ac880c62d718ed46e944980b1d43cc8d3a
Jacob <malin146@umn.edu>
2025-03-03 14:51:08 -0600

    Royal Assassin

--- src/main.py
+++ src/main.py
@@ -4,15 +4,15 @@ def replace_newlines(text):
     return text.replace('\n', '<br><br>')
 
 if __name__ == "__main__":
-    TITLE = "Howl's Moving Castle"
-    AUTHOR = "Diana Wynne Jones"
-    SUMMARY = "Sophie has the great misfortune of being the eldest of three daughters, destined to fail miserably should she ever leave home to seek her fate. But when she unwittingly attracts the ire of the Witch of the Waste, Sophie finds herself under a horrid spell that transforms her into an old lady. Her only chance at breaking it lies in the ever-moving castle in the hills: the Wizard Howl's castle.\nTo untangle the enchantment, Sophie must handle the heartless Howl, strike a bargain with a fire demon, and meet the Witch of the Waste head-on. Along the way, she discovers that there's far more to Howl—and herself—than first meets the eye.\nIn this giant jigsaw puzzle of a fantasy, people and things are never quite what they seem. Destinies are intertwined, identities exchanged, lovers confused. The Witch has placed a spell on Howl. Does the clue to breaking it lie in a famous poem? And what will happen to Sophie Hatter when she enters Howl's castle?"
-    PREFACE_SKIP = 7
-    POSTFACE_SKIP = 7
+    TITLE = "Royal Assassin"
+    AUTHOR = "Robin Hobb"
+    SUMMARY = "Fitz has barely survived his first hazardous mission as king’s assassin. Battered and bitter, he vows to abandon his oath to King Shrewd, remaining in the distant mountains. But love and events of terrible urgency draw him back to the court at Buckkeep, and into the deadly intrigues of the royal family.\nRenewing their vicious attacks on the coast, the Red-Ship Raiders leave burned-out villages and demented victims in their wake. The kingdom is also under assault from within, as treachery threatens the throne of the ailing king. In this time of great danger, the fate of the kingdom may rest in Fitz’s hands—and his role in its salvation may require the ultimate sacrifice."
+    PREFACE_SKIP = 4
+    POSTFACE_SKIP = 1
 
     HTML_PATH = "books/" + TITLE + ".html"
     with open(HTML_PATH, "wb") as out:
-        BOOK_PATH = "books/" + TITLE + ".pdf"
+        BOOK_PATH = "pdf/" + TITLE + ".pdf"
         template = [
             read_all("html/Template_1.html"),
             read_all("html/Template_2.html"),
